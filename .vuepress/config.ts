ac9ea1c582587d908e48fadde4a69b6e9345fa9f
13akstjq <13akstjq@gmail.com>
2022-11-08 16:51:54 +0900

    ad commit

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -56,6 +56,7 @@ export default defineUserConfig({
   bundler: viteBundler(),
   base: "/TIL/",
   head: [
+    ["meta", { name: "google-site-verification", content: "pvvSTvZm_50xq7R_E4wMghRYyaoQ0Of-Hf7MvqBbj8M" }],
     ["link", { rel: "apple-touch-icon", sizes: "180x180", href: "/TIL/assets/favicons/apple-touch-icon.png" }],
     ["link", { rel: "icon", type: "image/png", sizes: "32x32", href: "/TIL/assets/favicons/favicon-32x32.png" }],
     ["link", { rel: "icon", type: "image/png", sizes: "16x16", href: "/TIL/assets/favicons/favicon-16x16.png" }],
